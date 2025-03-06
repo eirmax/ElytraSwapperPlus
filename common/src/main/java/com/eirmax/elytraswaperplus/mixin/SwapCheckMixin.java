119ecc43f2ae7735478353eef2c995c12ea2ebeb
eirmax <eirmax8@gmail.com>
2025-03-06 22:43:10 +0700

    fix mixin

--- common/src/main/java/com/eirmax/elytraswaperplus/mixin/SwapCheckMixin.java
+++ common/src/main/java/com/eirmax/elytraswaperplus/mixin/SwapCheckMixin.java
@@ -11,14 +11,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ClientPlayerEntity.class)
 public class SwapCheckMixin {
 
-    @Inject(method = "tickMovement", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;checkGliding()Z", shift = At.Shift.AFTER))
+    @Inject(method = "tickMovement", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;isFallFlying()Z", shift = At.Shift.AFTER))
     public void swapToElytra(CallbackInfo callbackInfo) {
         if (!KeyBind.enabled) {
             return;
         }
         ClientPlayerEntity player = (ClientPlayerEntity) (Object) this;
         if (!player.isOnGround() &&
-                !player.isTouchingWater() &&
+                !player.isFallFlying() &&
                 !player.hasStatusEffect(StatusEffects.LEVITATION) &&
                 player.getVelocity().y < 0) {
             SwapUtil.tryWearElytra(player);
