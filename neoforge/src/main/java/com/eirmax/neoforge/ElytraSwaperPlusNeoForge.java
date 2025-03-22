98412136d50e2e29e525c9f8214ed66b54981ab1
eirmax <eirmax8@gmail.com>
2025-03-22 16:47:34 +0700

    update git

--- neoforge/src/main/java/com/eirmax/neoforge/ElytraSwaperPlusNeoForge.java
+++ neoforge/src/main/java/com/eirmax/neoforge/ElytraSwaperPlusNeoForge.java
@@ -3,10 +3,13 @@ package com.eirmax.neoforge;
 import com.eirmax.elytraswaperplus.ElytraSwaperPlus;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
 
 @Mod(ElytraSwaperPlus.MODID)
 public final class ElytraSwaperPlusNeoForge {
     public ElytraSwaperPlusNeoForge(IEventBus bus) {
        ElytraSwaperPlus.init();
+
+
     }
 }
