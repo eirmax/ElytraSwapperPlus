27d83750902fe9844f9bed166bef75d78a03aa1a
eirmax <eirmax8@gmail.com>
2025-03-22 16:53:53 +0700

    fix name

--- fabric/src/main/java/com/eirmax/fabric/client/KeyBindingRegistry.java
+++ fabric/src/main/java/com/eirmax/fabric/client/KeyBindingRegistry.java
@@ -6,7 +6,7 @@ import org.lwjgl.glfw.GLFW;
 
 public class KeyBindingRegistry {
     public static void init() {
-        registerKeyBind("key.elytraswapplus.swap", GLFW.GLFW_KEY_R, "category.elytraswapplus");
+        registerKeyBind("key.elytraswaperplus.swap", GLFW.GLFW_KEY_R, "category.elytraswaperplus");
     }
 
     public static KeyMapping registerKeyBind(String translationKeyName, int defaultKeyId, String category) {
--- neoforge/src/main/java/com/eirmax/neoforge/ElytraSwaperPlusNeoForge.java
+++ neoforge/src/main/java/com/eirmax/neoforge/ElytraSwaperPlusNeoForge.java
@@ -3,7 +3,6 @@ package com.eirmax.neoforge;
 import com.eirmax.elytraswaperplus.ElytraSwaperPlus;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
 
 @Mod(ElytraSwaperPlus.MODID)
 public final class ElytraSwaperPlusNeoForge {
