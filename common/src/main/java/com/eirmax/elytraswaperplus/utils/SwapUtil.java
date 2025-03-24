2b9fa7a208786adfe8bb89e33291a69b7aff3fcf
eirmax <eirmax8@gmail.com>
2025-03-24 12:21:49 +0700

    fix swap finally

--- common/src/main/java/com/eirmax/elytraswaperplus/utils/SwapUtil.java
+++ common/src/main/java/com/eirmax/elytraswaperplus/utils/SwapUtil.java
@@ -9,13 +9,96 @@ import net.minecraft.world.item.Items;
 public class SwapUtil {
     public static void tryWearElytra(Player player) {
         ItemStack chestplate = player.getItemBySlot(EquipmentSlot.CHEST);
-        ItemStack elytra = ArmorHelperUtil.findElytra(player);
+        ItemStack elytra = findElytra(player);
 
         if (elytra != null && !chestplate.is(Items.ELYTRA)) {
             if (player.getInventory().getFreeSlot() != -1 || chestplate.isEmpty()) {
                 player.getInventory().add(chestplate);
             }
             player.setItemSlot(EquipmentSlot.CHEST, elytra);
+        }
+    }
+
+    public static void swap(Player player) {
+        ItemStack chest = player.getItemBySlot(EquipmentSlot.CHEST);
+        if (chest.is(Items.ELYTRA)) {
+            swapElytraToChest(player);
+        } else {
+            swapChestToElytra(player);
+        }
+    }
+
+    public static void swapChestToElytra(Player player) {
+        ItemStack chest = player.getItemBySlot(EquipmentSlot.CHEST);
+        ItemStack elytra = findElytra(player);
+
+        if (elytra != null) {
+            if (!chest.isEmpty()) {
+
+                if (player.getInventory().getFreeSlot() != -1) {
+                    player.getInventory().add(chest);
+                } else {
+                    player.drop(chest, false);
+                }
+            }
+            player.getInventory().removeItem(elytra);
+            player.setItemSlot(EquipmentSlot.CHEST, elytra);
+        }
+    }
+
+    public static void swapElytraToChest(Player player) {
+        ItemStack currentChest = player.getItemBySlot(EquipmentSlot.CHEST);
+        ItemStack bestChestplate = ArmorHelperUtil.getBestChestplate(player);
+
+        if (bestChestplate != null) {
+            if (currentChest.is(Items.ELYTRA)) {
+                player.setItemSlot(EquipmentSlot.CHEST, ItemStack.EMPTY);
+                if (player.getInventory().getFreeSlot() != -1) {
+                    player.getInventory().add(currentChest);
+                } else {
+                    player.drop(currentChest, false);
+                }
+            }
+
+            for (int i = 0; i < player.getInventory().getContainerSize(); i++) {
+                ItemStack stack = player.getInventory().getItem(i);
+                if (stack.is(bestChestplate.getItem())) {
+                    player.getInventory().removeItem(stack);
+                    break;
+                }
+            }
+
+            player.setItemSlot(EquipmentSlot.CHEST, bestChestplate);
+        }
+    }
+    public static void swapToBestChestplate(Player player) {
+        ItemStack currentChest = player.getItemBySlot(EquipmentSlot.CHEST);
+        ItemStack bestChestplate = ArmorHelperUtil.getBestChestplate(player);
+
+        if (bestChestplate != null && !currentChest.is(bestChestplate.getItem())) {
+            if (currentChest.is(Items.ELYTRA)) {
+                player.getInventory().add(currentChest);
+            }
+            player.setItemSlot(EquipmentSlot.CHEST, bestChestplate);
+        }
+    }
+
+
+    public static void swapChestplateToSecondSlot(Player player) {
+        ItemStack chest = player.getItemBySlot(EquipmentSlot.CHEST);
+        ItemStack secondSlotItem = player.getInventory().getItem(1);
+
+        if (chest.is(Items.ELYTRA)) {
+            if (secondSlotItem.is(Items.ELYTRA)) {
+                player.getInventory().removeItem(secondSlotItem);
+                player.setItemSlot(EquipmentSlot.CHEST, secondSlotItem);
+            }
+        } else {
+            if (secondSlotItem.is(chest.getItem())) {
+
+                player.getInventory().removeItem(secondSlotItem);
+                player.setItemSlot(EquipmentSlot.CHEST, secondSlotItem);
+            }
         }
     }
 
@@ -26,28 +109,5 @@ public class SwapUtil {
             }
         }
         return null;
-    }
-
-
-    public static void swapChestToElytra(Player player) {
-        ItemStack chest = player.getItemBySlot(EquipmentSlot.CHEST);
-        ItemStack elytra = findElytra(player);
-
-        if (elytra != null) {
-            player.getInventory().add(chest);
-            player.setItemSlot(EquipmentSlot.CHEST, elytra);
-            player.getInventory().removeItem(elytra);
-        }
-    }
-    public static void swapToBestChestplate(Player player) {
-        ItemStack currentChest = player.getItemBySlot(EquipmentSlot.CHEST);
-        ItemStack bestChestplate = ArmorHelperUtil.getBestChestplate(player);
-
-        if (bestChestplate != null && !currentChest.is(bestChestplate.getItem())) {
-            if (player.getInventory().getFreeSlot() != -1 || currentChest.isEmpty()) {
-                player.getInventory().add(currentChest);
-            }
-            player.setItemSlot(EquipmentSlot.CHEST, bestChestplate);
-        }
     }
 }
