e5f4b58f72de2688476e8b3b4150a1b6441b9f87
eirmax <eirmax8@gmail.com>
2025-03-26 12:13:16 +0700

    update mixin start cloth config

--- common/src/main/java/com/eirmax/elytraswaperplus/mixin/StopFlying.java
+++ common/src/main/java/com/eirmax/elytraswaperplus/mixin/StopFlying.java
@@ -7,12 +7,16 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Player.class)
 public class StopFlying {
+
+    @Unique
+    private static double higth_falling = 3.0;
     @Inject(method = "tick", at = @At("TAIL"))
     public void onTickMovementEnd(CallbackInfo ci) {
         Player player = (Player) (Object) this;
@@ -20,7 +24,7 @@ public class StopFlying {
 
         if (!player.isCreative() && !player.hasEffect(MobEffects.SLOW_FALLING)) {
             if (!player.isInWater() && !player.isInLava() &&
-                    player.getDeltaMovement().y < 2 &&
+                    player.getDeltaMovement().y < higth_falling &&
                     !player.isFallFlying()) {
 
                 if (!chestItem.is(Items.ELYTRA)) {
@@ -30,7 +34,7 @@ public class StopFlying {
                 if (player.isFallFlying() && !chestItem.is(Items.ELYTRA)) {
                     SwapUtil.tryWearElytra(player);
                 }
-                if (player.getDeltaMovement().y < 2 && chestItem.is(Items.ELYTRA) && !player.isFallFlying()) {
+                if (player.getDeltaMovement().y < higth_falling && chestItem.is(Items.ELYTRA) && !player.isFallFlying()) {
                     player.startFallFlying();
                 }
             }
