0f89de45691dd4da73eff0c3ce62130dffaa4ebe
eirmax <eirmax8@gmail.com>
2025-04-20 18:40:47 +0700

    update enchanted system 1.21.1

--- common/src/main/java/com/eirmax/elytraswaperplus/utils/ArmorHelperUtil.java
+++ common/src/main/java/com/eirmax/elytraswaperplus/utils/ArmorHelperUtil.java
@@ -1,12 +1,16 @@
 package com.eirmax.elytraswaperplus.utils;
 
 
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.Enchantments;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class ArmorHelperUtil {
@@ -15,7 +19,7 @@ public class ArmorHelperUtil {
         int highestScore = -1;
         for (int i = 0; i < player.getInventory().getContainerSize(); i++) {
             ItemStack stack = player.getInventory().getItem(i);
-            if (isChestplate(stack) && !stack.is(Items.ELYTRA)) {
+            if (isChestplate(stack, player) && !stack.is(Items.ELYTRA)) {
                 int score = calculateScore(stack);
                 if (score > highestScore) {
                     highestScore = score;
@@ -26,14 +30,12 @@ public class ArmorHelperUtil {
         return best;
     }
 
-    private static boolean isChestplate(ItemStack stack) {
-        return stack.getItem() instanceof ArmorItem &&
-                ((ArmorItem) stack.getItem()).getEquipmentSlot() == EquipmentSlot.CHEST;
+    private static boolean isChestplate(ItemStack stack, Player player) {
+        return player.getEquipmentSlotForItem(stack) == EquipmentSlot.CHEST;
     }
 
     private static int calculateScore(ItemStack stack) {
         AtomicInteger score = new AtomicInteger();
-
 
         if (stack.getItem() == Items.NETHERITE_CHESTPLATE) score.addAndGet(6);
         else if (stack.getItem() == Items.DIAMOND_CHESTPLATE) score.addAndGet(5);
@@ -41,8 +43,25 @@ public class ArmorHelperUtil {
         else if (stack.getItem() == Items.CHAINMAIL_CHESTPLATE) score.addAndGet(3);
         else if (stack.getItem() == Items.GOLDEN_CHESTPLATE) score.addAndGet(2);
         else if (stack.getItem() == Items.LEATHER_CHESTPLATE) score.addAndGet(1);
+
         if (stack.isEnchanted()) score.addAndGet(2);
-        stack.getEnchantments().entrySet().forEach(e -> score.addAndGet(e.getIntValue() * 2));
+        Map<ResourceKey<Enchantment>, Integer> enchantmentPoints = new HashMap<>();
+        enchantmentPoints.put(Enchantments.PROTECTION, 9);
+        enchantmentPoints.put(Enchantments.UNBREAKING, 7);
+        enchantmentPoints.put(Enchantments.MENDING, 8);
+        enchantmentPoints.put(Enchantments.PROJECTILE_PROTECTION, 4);
+        enchantmentPoints.put(Enchantments.BLAST_PROTECTION, 3);
+        enchantmentPoints.put(Enchantments.FIRE_PROTECTION, 5);
+        enchantmentPoints.put(Enchantments.THORNS, 6);
+        stack.getEnchantments().entrySet().forEach(e -> {
+            Enchantment enchantment = e.getKey().value();
+            int level = e.getValue();
+            Integer points = enchantmentPoints.get(enchantment);
+            if (points != null) {
+                score.addAndGet(points * level);
+            }
+        });
+
         return score.get();
     }
 }
