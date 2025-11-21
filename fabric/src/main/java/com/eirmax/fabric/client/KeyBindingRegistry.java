7548eb1f10c71ccd303295eaf4b8f9edaa980f0b
eirmax <eirmax8@gmail.com>
2025-11-21 12:20:11 +0700

    Fix new minecraft keybind category Fabric

--- fabric/src/main/java/com/eirmax/fabric/client/KeyBindingRegistry.java
+++ fabric/src/main/java/com/eirmax/fabric/client/KeyBindingRegistry.java
@@ -1,16 +1,20 @@
 package com.eirmax.fabric.client;
 
+import com.eirmax.elytraswaperplus.ElytraSwapperPlus;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.KeyMapping;
+import net.minecraft.resources.ResourceLocation;
 import org.lwjgl.glfw.GLFW;
 
 public class KeyBindingRegistry {
+    public static final KeyMapping.Category ELYTRA_SWAP_PLUS = new KeyMapping.Category(ResourceLocation.fromNamespaceAndPath(ElytraSwapperPlus.MODID, "category.elytraswapplus"));
+
     public static void init() {
-        registerKeyBind("key.elytraswapplus.swap", GLFW.GLFW_KEY_R, "category.elytraswapplus");
-        registerKeyBind("key.elytraswapplus.auto_swap", GLFW.GLFW_KEY_B, "category.elytraswapplus");
+        registerKeyBind("key.elytraswapplus.swap", GLFW.GLFW_KEY_R, ELYTRA_SWAP_PLUS);
+        registerKeyBind("key.elytraswapplus.auto_swap", GLFW.GLFW_KEY_B, ELYTRA_SWAP_PLUS);
     }
 
-    public static KeyMapping registerKeyBind(String translationKeyName, int defaultKeyId, String category) {
+    public static KeyMapping registerKeyBind(String translationKeyName, int defaultKeyId, KeyMapping.Category category) {
         KeyMapping bind = new KeyMapping(translationKeyName, defaultKeyId, category);
         KeyBindingHelper.registerKeyBinding(bind);
         ClientFabricKeybindEvent.keyMappings.add(bind);
